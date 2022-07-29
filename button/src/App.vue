658829c38eb3812b68eefaad1e7dcffcef4b1397
TCIano <740409310@qq.com>
2022-07-29 22:20:28 +0800

    button

---
+++ button/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <!-- 自定义 内容 -->
+    <buttonVue type="primary" title="nihao" />
+    <hr />
+    <!-- 自定义宽高 -->
+    <buttonVue type="info" title="按钮2" width="220px" height="50px" />
+
+    <!-- 自定义颜色 -->
+    <buttonVue title="按钮2" width="220px" height="50px" color="#f8091" />
+    <!-- 自定义边框 -->
+    <buttonVue title="按钮2" width="220px" height="50px" color="#f8091" />
+  </div>
+</template>
+
+<script>
+import buttonVue from "./components/buttonVue.vue";
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    buttonVue,
+  },
+  created() {},
+
+  methods: {},
+};
+</script>
+
+<style scoped></style>
---
+++ button/src/components/buttonVue.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <button
+      :class="type"
+      :style="{
+        width: width,
+        height: height,
+        backgroundColor: color,
+      }"
+    >
+      <span>{{ title }}</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    type: {
+      type: String,
+    },
+    title: {
+      type: String,
+    },
+    width: {
+      type: String,
+    },
+    height: {
+      type: String,
+    },
+    color: {
+      type: String,
+    },
+  },
+  created() {},
+
+  methods: {},
+};
+</script>
+
+<style scoped>
+button {
+  margin: 5px;
+}
+.primary {
+  background-color: green;
+}
+.info {
+  background-color: blue;
+}
+.warning {
+  background-color: pink;
+}
+.warning {
+  background-color: red;
+}
+</style>
